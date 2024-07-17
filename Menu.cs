ae375aa63a3e92e64de51033c8ec07c8d2dbd5a0
Victor Samuel Oliveira <victorsamueloliveira59@gmail.com>
2024-07-16 22:31:15 -0300

    Criando um Editor Html

---
+++ Menu.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace EditorHtml
+{
+    class Program
+    {
+        public static class Menu
+        {
+            public static void Show()
+            {
+                Console.Clear();
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                
+            }
+        }
+    }
+}	
