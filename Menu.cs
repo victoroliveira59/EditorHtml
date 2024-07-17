cada44d4059ef00a65d486e9c73be349969e4eaa
Victor Samuel Oliveira <victorsamueloliveira59@gmail.com>
2024-07-16 22:51:35 -0300

    Criando uma caixa para adicionar o testo

--- Menu.cs
+++ Menu.cs
@@ -2,17 +2,41 @@ using System;
 
 namespace EditorHtml
 {
-    class Program
+    public static class Menu
     {
-        public static class Menu
+        public static void Show()
         {
-            public static void Show()
-            {
-                Console.Clear();
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Red;
+            DrawScreen();
+        }
+        static void DrawScreen()
+        {
+        Line();
+        Column();
+        Line();
 
-                
+        }
+        static void Line()
+        {
+             Console.Write("+");
+            for (int i = 0; i <= 30; i++ )
+                Console.Write("-");
+            Console.Write("+");
+            Console.Write("\n");
+        }
+        static void Column()
+        {
+            for (int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= 30; i++ )
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("|");
+                Console.Write("\n");
             }
         }
     }
